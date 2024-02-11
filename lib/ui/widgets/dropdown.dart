29b9074c9bbfeaec9f7eb7f92500227aa662f6ed
Rensi Meila Yulvinata <54298132+rensimeila04@users.noreply.github.com>
2024-02-11 23:35:27 +0700

    Create dropdown widget

---
+++ lib/ui/widgets/dropdown.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:wanderind/shared/theme.dart';
+import 'package:iconsax/iconsax.dart';
+
+class DropdownCustom extends StatefulWidget {
+  const DropdownCustom({super.key});
+
+  @override
+  State<DropdownCustom> createState() => _DropdownCustomState();
+}
+
+class _DropdownCustomState extends State<DropdownCustom> {
+  String _selectedLanguage = 'Select Language';
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56,
+      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(36),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton<String>(
+                value: _selectedLanguage,
+                icon: Icon(Iconsax.arrow_down_1),
+                iconSize: 24,
+                elevation: 16,
+                style: grayTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: regular
+                ),
+                onChanged: (dynamic newValue) {
+                  setState(() {
+                    _selectedLanguage = newValue.toString();
+                  });
+                },
+                items: <String>[
+                  'Select Language',
+                  'English',
+                  'Bahasa Indonesia'
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
