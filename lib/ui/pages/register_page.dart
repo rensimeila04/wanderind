c8d174813b57bca54bdcaf3d8bbba8414783be79
Rensi Meila Yulvinata <54298132+rensimeila04@users.noreply.github.com>
2024-02-13 22:36:09 +0700

    Edit Register Page

--- lib/ui/pages/register_page.dart
+++ lib/ui/pages/register_page.dart
@@ -9,134 +9,136 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/icon_logo.png',
-              width: 126,
-              height: 118.85,
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            Text(
-              "Let's get started",
-              style:
-                  blackTextStyle.copyWith(fontSize: 18, fontWeight: medium),
-            ),
-            SizedBox(
-              height: 32,
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 24),
-              child: Column(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/icon_logo.png',
+                width: 126,
+                height: 118.85,
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Text(
+                "Let's get started",
+                style:
+                    blackTextStyle.copyWith(fontSize: 18, fontWeight: medium),
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 24, vertical: 18),
+                          hintText: "Email/Phone Number",
+                          hintStyle: grayTextStyle.copyWith(
+                              fontSize: 16, fontWeight: regular),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(36)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: const Color(0xFF1A80E4)),
+                              borderRadius: BorderRadius.circular(36))),
+                      style: blackTextStyle.copyWith(
+                          fontSize: 16, fontWeight: regular),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    ButtonBig(
+                      buttonText: 'Create Account',
+                      onPressed: () {},
+                    ),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Divider(),
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Text(
+                          'Or',
+                          style: grayTextStyle.copyWith(
+                              fontSize: 16, fontWeight: regular),
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Expanded(
+                          child: Divider(),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.symmetric(
-                            horizontal: 24, vertical: 18),
-                        hintText: "Email/Phone Number",
-                        hintStyle: grayTextStyle.copyWith(
-                            fontSize: 16, fontWeight: regular),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(36)),
-                        focusedBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: const Color(0xFF1A80E4)),
-                            borderRadius: BorderRadius.circular(36))),
-                    style: blackTextStyle.copyWith(
-                        fontSize: 16, fontWeight: regular),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  ButtonBig(
-                    buttonText: 'Create Account',
+                  IconButton(
                     onPressed: () {},
+                    icon: Brand(
+                      Brands.facebook,
+                      size: 40,
+                    ),
                   ),
-                  SizedBox(
-                    height: 32,
+                  IconButton(
+                    onPressed: () {},
+                    icon: Brand(
+                      Brands.google,
+                      size: 40,
+                    ),
                   ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Divider(),
-                      ),
-                      SizedBox(
-                        width: 16,
-                      ),
-                      Text(
-                        'Or',
-                        style: grayTextStyle.copyWith(
-                            fontSize: 16, fontWeight: regular),
-                      ),
-                      SizedBox(
-                        width: 16,
-                      ),
-                      Expanded(
-                        child: Divider(),
-                      )
-                    ],
-                  )
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      BoxIcons.bxl_apple,
+                      size: 40,
+                    ),
+                  ),
                 ],
               ),
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Brand(
-                    Brands.facebook,
-                    size: 40,
+              SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account?",
+                    style: captionTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: regular,
+                    ),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Brand(
-                    Brands.google,
-                    size: 40,
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/sign-in');
+                    },
+                    child: Text('Sign In', style: blueTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: semiBold
+                    )),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    BoxIcons.bxl_apple,
-                    size: 40,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Already have an account?",
-                  style: captionTextStyle.copyWith(
-                    fontSize: 16,
-                    fontWeight: regular,
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/sign-in');
-                  },
-                  child: Text('Sign In', style: blueTextStyle.copyWith(
-                    fontSize: 16,
-                    fontWeight: semiBold
-                  )),
-                ),
-              ],
-            )
-          ],
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
